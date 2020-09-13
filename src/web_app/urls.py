3c4b86c0514c5dbd58dece30c3b74ebf265dd5c2
Anatoliy Kurinnyy <one.quaker@gmail.com>
2020-09-13 22:40:56 +0300

    api doc added

--- src/web_app/urls.py
+++ src/web_app/urls.py
@@ -18,9 +18,25 @@ from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import serializers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title='Order API',
+      default_version='v1',
+      description='Order description',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-redoc'),
     re_path(r'api/(?P<version>[v1|v2]+)/', include('order_rest.urls')),
 ]
 
